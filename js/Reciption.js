9aead994f06e84da839445424d4d7570e6f879e2
hensamnang <samnang.hen@student.passerellesnumeriques.org>
2019-12-21 19:16:37 +0700

    chang jquery

--- js/Reciption.js
+++ js/Reciption.js
@@ -4,7 +4,13 @@ function getUrl() {
 }
 $(document).ready(function () {
     requestAPi();
+    //hide and show number of person
+    $('#number').hide();
+    $('#person').hide();
+
     $('#reciption').on('change', () => {
+    $('#number').show();
+    $('#person').show();
         var chooseRecipes = $('#reciption').val();
         getDataUrl(chooseRecipes);
     });
@@ -22,88 +28,116 @@ function getRecipes(rec) {
     allData = rec;
     var option = "";
     rec.forEach(element => {
-        option += `
-            <option value="${element.id}">${element.name}</option>
-        `;
+        option += `<option value="${element.id}">${element.name}</option>`;
     });
     $('#reciption').append(option);
 };
-//function for get element  from recipes 
+//function for get element  from recipes
+var numQuantity;
+var oldGuest ;
 var getDataUrl = (recip) => {
     allData.forEach(item => {
         if (item.id == recip) {
             getHead(item.name, item.iconUrl);
-            getIngredient(item.ingredients);
+            getDataIngredient(item);
             getStruction(item.instructions);
-            getNumber(item.nbGuests)
+            getNumber(item.nbGuests);
+
+            oldGuest= item.nbGuests;      
+            numQuantity = item;                
         };
     });
 };
-
 //function get name and iconUrl from recipes
 var getHead = (name, icon) => {
     var heading = "";
     heading += `
     <div class="col-3"></div>
-    <div class="col-3" class=" btn text-right"><h5>${name}</h5></div>
+    <div class="col-3" class=" btn text-right"><h2>${name}</h2></div>
     <div class="col-3"><img src="${icon}" width="70%"></div>
     <div class="col-3"></div>
-        `;
-    $('#nameData').html(heading)
-}
-
-function getNumber(number){
+    `;
+    $('#nameData').html(heading);
+};
+//Progress bar for calulate number
+var  getNumber = (number) =>{
     var multi = "";
         multi +=`
-        <div class="col-4"></div>
-            <div class="col-4">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <button class="btn btn-primary" type="button" id="minus">&minus;</button>
-                    </div>
-                    <input type="number" class="form-control text-center" value="${number}" disabled id="member" max="15"
-                        min="0">
-                    <div class="input-group-append">
-                        <button class="btn btn-success" type="button" id="add">&#x2b;</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-4"></div>
+        <input  class="form-control text-center" value="${number}" disabled id="execute" max="15"min="0">
         `;
-        $('#number').html(multi)
-}
-
-
-//function get ingredients from recipes
-var getIngredient = (ingre) => {
-    ingre.forEach(ingredien => {
-        getDataIngredient(ingredien);
-    });
+    $('#calculate').html(multi);
 };
-// var getDataIngredient = (inger) => {
-//     var result = " ";
-//     result += `
-//     <tr>
-//         <td><img src ="${inger.iconUrl}" width= "10%"></td>
-//         <td>${inger.name}</td>
-//         <td>${inger.quantity}</td>
-//         <td>${inger.unit[0].toLowerCase()}</td>
-//     </tr>
-//     `;
-//     $('#data').append(result);
-// }
-
+//calculate number of quest when click button
+var numbers ; 
+$('#add').on('click', function(){
+    var addNumber = $('#execute').val();
+   var increase = parseInt(addNumber) + 1;
+   if(increase <= 15 ){
+     $('#execute').val(increase);
+     executeNumber(increase);
+   }
+});
+$('#minus').on('click', function(){
+    var minusNumber = $('#execute').val();
+   var decrease = parseInt(minusNumber) - 1;
+   if(decrease >= 1){
+     $('#execute').val(decrease); 
+    executeNumber(decrease);
+   }
+});
+var executeNumber = (num) =>{
+    var getQuantity= "";
+    numQuantity.ingredients.forEach(quant => {
+        getQuantity +=`
+        <tr class ="text-center">
+        <td><img src ="${quant.iconUrl}" width= "17%"></td>
+        <td>${quant.quantity * num/oldGuest}</td>
+        <td>${quant.unit[0].toLowerCase()}</td>
+        <td>${quant.name}</td>
+        </tr>
+        `;
+        $('#data').html(getQuantity);
+    });
+    return parseInt(num);
+};
+//create for get data from recipes
+var getDataIngredient = (ing) => {
+    // create for heading
+    var ingredients = "";
+    ingredients += `<h4 class="text-center">Ingredients</h4>`;
+    //result ingredients from recipes
+    var result = " ";
+    ing.ingredients.forEach(ingre => {
+        result += `
+        <tr class ="text-center">
+        <td><img src ="${ingre.iconUrl}" width= "17%"></td>
+        <td>${ingre.quantity}</td>
+        <td>${ingre.unit[0].toLowerCase()}</td>
+        <td>${ingre.name}</td>
+        </tr>
+        `;
+        $('#data').html(result);
+        $('#ingredients').html(ingredients);
+   });
+};
 //function cut step from instruction
 var getStruction = (instruct) => {
     var instrunction =instruct.split("<step>");
-    var output= "";
+    //create for verticalline
+    var verticalline= "";
+    verticalline +=` <div class="col-2"  style="border-right: 2px solid rgb(119, 119, 240);height: 300px;"></div>`;
+    $('#vertical_line').html(verticalline);
+    //create for instruction 
+    var instruction = "";
+        instruction += `<h4  class="text-left">Instruction</h4>`; 
+        $('#instructions').html(instruction);
+    //create for put intruction
+    var outputInstruction= "";
     for(var i = 1; i < instrunction.length; i++){
-        output+= `
+        outputInstruction+= `
             <tr class= "text-left">
-                <td><strong class=" btn text-primary">step</strong>${i} : <br> ${instrunction[i]};</td>
+                <td><strong class=" btn text-primary">step</strong>${i}<br> ${instrunction[i]};</td>
             </tr>
-        `;
-           
-        }
-        $('#instruc').html(output);
-    }
+            `;}
+        $('#instruc').html(outputInstruction);
+    };
